fb07c55429f7e09b741d2fc4f9e1e0c2264c64ac
Blanchard Dorian <blanchardorian@gmail.com>
2024-10-18 12:32:38 +0200

    Adding the Parser to export clause

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,12 @@
-import File from "./model/file/File.class.js";
-import Folder from "./model/folder/Folder.class.js";
-import { ServerFile, ServerFolder } from "./model/server.js";
+import File from "./model/file/File.class.mjs";
+import Folder from "./model/folder/Folder.class.mjs";
+import Parser from "./model/file/parser.class.mjs";
+import { ServerFile, ServerFolder } from "./model/server.mjs";
 
 export {
     File,
     ServerFile,
     Folder,
-    ServerFolder
+    ServerFolder,
+    Parser
 }
